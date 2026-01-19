dfb7b8ceeb3508fe934babd1a5e26e0459e1cbfb
Pratiksha Dikkar <64153770+pratikshadikkar@users.noreply.github.com>
2026-01-19 18:01:44 +0530

    Create run.tf

---
+++ run.tf
@@ -0,0 +1,37 @@
+resource "google_cloud_run_service" "agent_tools" {
+  name     = "${var.agent_name}-tools-${var.environment}"
+  location = var.region
+
+  template {
+    metadata {
+      annotations = {
+        "autoscaling.knative.dev/maxScale" = "10"
+      }
+    }
+
+    spec {
+      service_account_name = google_service_account.agent_sa.email
+
+      containers {
+        image = var.tool_image
+
+        env {
+          name  = "AGENT_CONFIG_SECRET"
+          value = google_secret_manager_secret.agent_config.secret_id
+        }
+
+        resources {
+          limits = {
+            cpu    = "1"
+            memory = "512Mi"
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
